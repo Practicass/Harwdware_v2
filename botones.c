47caa66ec5f0081b236ff837a9eb2d318965bb2d
Miguel Aréjula <arejula10@gmail.com>
2023-12-09 22:19:29 +0100

    botones comentado

--- botones.c
+++ botones.c
@@ -3,9 +3,12 @@
 
 #include "botones.h"
 
-enum ESTADO boton_1, boton_2 ;
-void (*callback_alarma_activar)();
 
+enum ESTADO boton_1, boton_2 ; 						// Estado de los botones
+void (*callback_alarma_activar)();					// Puntero a la funcion de activacion de la alarma de comprobacion
+
+
+// Inicializa los botones
 void botones_init(void (*callback_fifo_encolar_param)(), void (*callback_alarma_activar_param)()){
 	boton_1 = NO_PULSADO;
 	boton_2 = NO_PULSADO;
@@ -15,35 +18,37 @@ void botones_init(void (*callback_fifo_encolar_param)(), void (*callback_alarma_
 	
 }
 
+// Modifica el estado de los botones y programa la alarma de comprobacion
 void botones_pulsar(uint32_t auxData){
 
 	switch (auxData) {
 		case 1:
-			boton_1 = PULSADO;
-			callback_alarma_activar(BOTON_EINT1_ALARM,0x80000064,0);
+			boton_1 = PULSADO;											//cambia el estado del boton
+			callback_alarma_activar(BOTON_EINT1_ALARM,0x80000064,0); 	//programa la alarma de comprobacion
 			break;
 		case 2:
-			boton_2 = PULSADO;
-			callback_alarma_activar(BOTON_EINT2_ALARM,0x80000064,0);
+			boton_2 = PULSADO;											//cambia el estado del boton
+			callback_alarma_activar(BOTON_EINT2_ALARM,0x80000064,0); 	//programa la alarma de comprobacion
 			break;
 	}
 }
 
+//Comprueba si los botones estan pulsados
 void botones_monitorizar(EVENTO_T ID_evento){
 	
 	switch (ID_evento) {
 		case BOTON_EINT1_ALARM:
 			if(!eint1_hold()){
-				callback_alarma_activar(BOTON_EINT1_ALARM, 0,0);
-				boton_1 = NO_PULSADO;
+				callback_alarma_activar(BOTON_EINT1_ALARM, 0,0);  		//desactiva la alarma de comprobacion
+				boton_1 = NO_PULSADO;									//cambia el estado del boton	
 				eint1_enable();
 			}
 			
 			break;
 		case BOTON_EINT2_ALARM:
 			if(!eint2_hold()){
-				callback_alarma_activar(BOTON_EINT2_ALARM, 0,0);
-				boton_1 = NO_PULSADO;
+				callback_alarma_activar(BOTON_EINT2_ALARM, 0,0);		//desactiva la alarma de comprobacion
+				boton_1 = NO_PULSADO;									//cambia el estado del boton
 				eint2_enable();
 			}
 
--- botones.h
+++ botones.h
@@ -6,16 +6,19 @@
 #include "gpio_hal.h"
 #include <inttypes.h>
 
+// Estados de los botones
 enum ESTADO{
     PULSADO,
     NO_PULSADO
 };
 
-
+// Inicializa los botones
 void botones_init(void (*callback_fifo_encolar_param)(), void (*callback_alarma_activar_param)());
 
+// Modifica el estado de los botones y programa la alarma de comprobacion
 void botones_pulsar(uint32_t auxData);
 
+//Comprueba si los botones estan pulsados
 void botones_monitorizar(EVENTO_T ID_evento);
 
 
